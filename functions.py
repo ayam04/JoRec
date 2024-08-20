cf72fdb67da861344c80036b7435ccfcbfd751e3
ayam04 <ayamullahkhan04@gmail.com>
2024-08-20 11:56:26 +0530

    upd

--- functions.py
+++ functions.py
@@ -4,32 +4,33 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-openai.api_key = os.getenv("OPENAI_KEY")
+client = openai.OpenAI(
+    api_key=os.getenv("OPENAI_KEY")
+)
 
 def evaluate_candidate(jd_text):
     if not jd_text:
         return "Job description text cannot be empty."
 
-    messages =  [
+    messages = [
         {"role": "system", "content": "You are a friendly and professional career advisor."},
         {"role": "user", "content": (
-            f"Please review the following job description and provide feedback, including a score out of 10 for each of the following criteria: Clarity, Inclusivity, Relevance, Structure, Company Branding, Performance Indicators, and Engagement. For each criterion, generate a two-line summary to justify the score./n Additionally, offer friendly suggestions for improvement where needed. Job Description: {jd_text}"
+            f"Please review the following job description and provide feedback, including a score out of 10 for each of the following criteria: Clarity, Inclusivity, Relevance, Structure, Company Branding, Performance Indicators, and Engagement. For each criterion, generate a two-line summary to justify the score.\n Additionally, offer friendly suggestions for improvement where needed. Job Description: {jd_text}"
         )}
     ]
 
-
     try:
-        response = openai.ChatCompletion.create(
+        response = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=messages,
             max_tokens=500,
             temperature=1
         )
-        return response.choices[0].message["content"].strip()
+        return response.choices[0].message.content
     except Exception as e:
         return f"An error occurred: {e}"
 
 if __name__ == "__main__":
-    job_text = "A data scienctist from bangalore with 5 years of experience in machine learning and deep learning."
+    job_text = "A data scientist from Bangalore with 5 years of experience in machine learning and deep learning."
     feedback = evaluate_candidate(job_text)
     print(feedback)
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 from functions import evaluate_candidate
 from pydantic import BaseModel
 import uvicorn
