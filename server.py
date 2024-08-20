f2716a7f486a0ed72bedd49cacc0da975a471e18
ayam04 <ayamullahkhan04@gmail.com>
2024-08-20 10:50:12 +0530

    upd

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI, Query
 from functions import evaluate_candidate
+from pydantic import BaseModel
 import uvicorn
 
 app = FastAPI()
 
+class JDAnalyzerRequest(BaseModel):
+    resume_text: str
+
 @app.post("/suggest-jd")
-async def suggest_candidate(resume_text: str = Query(...)):
-    return {'output': evaluate_candidate(resume_text)}
+async def suggest_candidate(jd_reqst: JDAnalyzerRequest):
+    resume_text = jd_reqst.resume_text
+    response = {'output': evaluate_candidate(resume_text)}
+    return response
 
 if __name__ == "__main__":
     uvicorn.run(app, host=8080)
